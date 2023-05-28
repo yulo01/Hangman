9ded65563928c9986d74e7324eb16db425017569
yulo01 <giorgi.kuliashvili.1@btu.edu.ge>
2023-05-28 19:47:40 +0400

    rules component update

--- src/components/Rules.js
+++ src/components/Rules.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import "./rules.css"
+import closeIcon from '../assets/close-icon.png'
+import { useState} from 'react'
 
-export default function rules() {
+export default function Rules() {
+  
+  const [closeRules, setCloseRules] = useState(true)
+
+  const showAndHide = () => {
+    setCloseRules(false)
+  }
+
+  if (!closeRules) {
+    return null; 
+  }
+
   return (
-    <div className='Rules'>
+    <div className={`Rules ${closeRules ? '' : 'hidden'}`}>
+      <button onClick={showAndHide} className="close">
+        <img src={closeIcon} alt="close-icon" className='close-icon' />
+      </button>
       <div>
         <p>ასოების გამოყენებით უნდა გამოიცანი სიტყვა</p>
         <p>ასოს შეშლისას, წაგებას ერთი ნაბიჯით უახლოვდები</p>
